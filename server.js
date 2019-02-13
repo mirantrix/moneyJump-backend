ca758a66921a54bc4cba93521ef984a8fc9bf5d4
User <mirantrix80@gmail.com>
2019-02-12 19:05:33 -0800

    Change the variable name to users and enpoint name to /api/users

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 
-app.get('/api/customers', (req, res) => {
+app.get('/api/users', (req, res) => {
 
-  var customers = [
+  var users = [
     {id: 1, firstName: ' Alex', lastName: 'Smith'},
     {id: 2, firstName: ' Carl', lastName: 'Sagan'},
     {id: 3, firstName: ' Elon', lastName: 'Musk'},
@@ -13,7 +13,7 @@ app.get('/api/customers', (req, res) => {
     {id: 6, firstName: ' Jerry', lastName: 'Seinfeld'}
   ];
 
-  res.json(customers);
+  res.json(users);
 
 })
 
